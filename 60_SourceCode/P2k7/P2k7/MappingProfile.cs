c1c1fc5fe803a36a729438733e8ce4f784afe2a0
novi82 <oritoka@gmail.com>
2020-11-20 16:34:42 +0700

    Main template

---
+++ 60_SourceCode/P2k7/P2k7/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace P2k7
+{
+    class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<GridEntryEntity, IDownloadItem>();
+            //CreateMap<GridEntryEntity, TfsDownloadItem>();
+            //CreateMap<GridEntryEntity, TfsGuildPathDownloadItem>();
+        }
+    }
+}
---
+++ 60_SourceCode/P2k7/P2k7/View/FrmMain.cs
@@ -0,0 +1,21 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2k7.View
+{
+    public partial class FrmMain : MaterialForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
